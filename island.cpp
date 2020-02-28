048e3594964c50f135168bc65d67673ca4b5a381
aabraamoovaa <55063807+aabraamoovaa@users.noreply.github.com>
2020-02-29 00:47:06 +0600

    Update island.cpp

--- island.cpp
+++ island.cpp
@@ -1,75 +1,54 @@
 #include <vector>  
 #include <iostream>
 #include <fstream>
-using namespace std;  
-
 
 int main()
 {
-	fstream f("1.txt");
+	std::fstream f("1.txt");
     int count = 0;// число чисел в файле
-	int temp;//Временная переменная
-
-	while (!f.eof())// пробегаем пока не встретим конец файла eof
+	int temp;
+	while (!f.eof())
 	{
-		f >> temp;//в пустоту считываем из файла числа
-		count++;// увеличиваем счетчик числа чисел
+		f >> temp;
+		count++;
 	}
- 
-		//Число чисел посчитано, теперь нам нужно понять сколько
-		//чисел в одной строке
-		//Для этого посчитаем число пробелов до знака перевода на новую строку 
- 
-	//Вначале переведем каретку в потоке в начало файла
-	f.seekg(0, ios::beg);
-	f.clear();
- 
-	//Число пробелов в первой строчке вначале равно 0
+	f.seekg(0, std::ios::beg);
 	int count_space = 0;
 	char symbol;
-	while (!f.eof())//на всякий случай цикл ограничиваем концом файла
+	while (!f.eof())
 	{
-		//теперь нам нужно считывать не числа, а посимвольно считывать данные
-		f.get(symbol);//считали текущий символ
-		if (symbol == ' ') count_space++;//Если это пробел, то число пробелов увеличиваем
-		if (symbol == '\n') break;//Если дошли до конца строки, то выходим из цикла
+		f.get(symbol);
+		if (symbol == ' ')
+			count_space++;
+		if (symbol == '\n')
+			break;
 	}
-	//cout << count_space << endl;
- 
-	//Опять переходим в потоке в начало файла
-	f.seekg(0, ios::beg);
-	f.clear();
- 
-	//Теперь мы знаем сколько чисел в файле и сколько пробелов в первой строке.
-	//Теперь можем считать матрицу.
-    int countland = 0;
-	int n = count / (count_space + 1);//число строк
-	int m = count_space + 1;//число столбцов на единицу больше числа пробелов
-	double **x;
-	x = new double*[n];
-	for (int i = 0; i<n; i++) x[i] = new double[m];
- 	
- 				    vector < vector <int> > vec(n, vector <int> (m));
-	//Считаем матрицу из файла
-	for (int i = 0; i < n; i++)
+	f.seekg(0, std::ios::beg);
+	int n = count / count_space;//число столбцов
+	int m = count_space;//число строк
+
+	int countland = 0; //число островов
+ 	std::vector < std::vector <int> > vec(n, std::vector <int> (m));
+	for (int i = 0; i < m; i++)
 	{	
-		cout<<"\n";
+		std::cout<<"\n";
 		{
-			for (int j = 0; j < m; j++)
+			for (int j = 0; j < n; j++)
 			{
-				f >> x[i][j];
-
-			    cout << x[i][j] << ' '; // Вывод элементов i строки вектора
-
-			    if ((x[i][j] == 1))
+				f >> vec[i][j];
+			    std::cout << vec[i][j] << ' ';
+			    if ((vec[i][j] == 1))
 			    {
 			    	countland++;
-               		    }
-
+			    
+			    if ((vec[i][j-1]==1)||((vec[i][j+1]==1))||(vec[i-1][j]==1)||(vec[i+1][j]==1))
+			    {
+			        countland = countland-1;
+			    };
+			    }
 	        }
 	    }
 	}
-    cout<<"\n"<<"Kol-vo land: "<<countland<<endl;
-
+    std::cout<<"\n"<<"Количество островов: "<<countland<< std::endl;
     return 0;
 }
