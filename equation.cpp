a1991b555a0bb89e0cac84e2d54b873d98024982
aabraamoovaa <55063807+aabraamoovaa@users.noreply.github.com>
2020-10-04 23:47:10 +0300

    Update equation.cpp

--- equation.cpp
+++ equation.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
 #include <cmath>
 #include <assert.h>
-using namespace std;
 
-int number_of_roots(double a, double b, double c, double* x1, double* x2)
+int discriminant(double a, double b, double c, double* D)
+{
+    *D = pow(b, 2) - 4*a*c;
+    return *D;
+}
+
+int number_of_roots(double a, double b, double c, double* D, double* x1, double* x2)
 {
 	assert(x1 != NULL);
     assert(x2 != NULL);
     assert(x1 != x2);
-    double D = pow(b, 2) - 4*a*c;
-    if(a != 0)
+    if (a != 0)
     {
-        if(D > 0)
+        if (*D > 0)
         {
-            *x1 = (-b + sqrt(D)) / (2*a);
-            *x2 = (-b - sqrt(D)) / (2*a);
             return 2;
         }
-        else if (D == 0) 
+        else if (*D == 0) 
         {
-            *x1 = -b / (2*a);
-            *x2 = *x1;
             return 1;
         } 
         else 
@@ -30,41 +30,76 @@ int number_of_roots(double a, double b, double c, double* x1, double* x2)
     } 
     else 
     {
-        if(b != 0)
+        if (b != 0)
         {
-            *x1 = -c / b;
             return 1;
         } 
     }
 }
+
+void roots(double a, double b, double c, double* D, double* x1, double* x2)
+{
+    assert(x1 != NULL);
+    assert(x2 != NULL);
+    assert(x1 != x2);
+    if (a != 0)
+    {
+        if (*D > 0)
+        {
+            *x1 = (-b + sqrt(*D)) / (2*a);
+            *x2 = (-b - sqrt(*D)) / (2*a);
+        }
+        else if (*D == 0) 
+        {
+            *x1 = -b / (2*a);
+            *x2 = *x1;
+        } 
+    } 
+    else 
+    {
+        if (b != 0)
+        {
+            *x1 = -c / b;
+        } 
+    }
+}
+
+void answer(double a, double b, double c, double& D, double& x1, double& x2)
+{
+    int number = 0;
+    number = number_of_roots(a, b, c, &D, &x1, &x2);
+    roots(a, b, c, &D, &x1, &x2);
+    switch(number)
+    {
+        case 0:
+            std::cout << "Нет действительных корней" << std::endl;
+        break;
+        case 1:
+            std::cout << "x = " << x1;
+        break;
+        case 2:
+            std::cout << "x1 = " << x1 << std::endl;
+            std::cout << "x2 = " << x2 << std::endl;
+        break;
+    }
+}
+
 int main()
 {
 	double a;
 	double b;
 	double c;
+    double D;
 	double x1;
 	double x2;
-	int number = 0;
 
-    cout << "Введите a: ";
-    cin >> a;
-    cout << "Введите b: ";
-    cin >> b;
-    cout << "Введите c: ";
-    cin >> c;
-    number = number_of_roots(a, b, c, &x1, &x2);
-    switch(number)
-    {
-    	case 0:
-        	std::cout << "Нет действительных корней" << endl;
-        break;
-    	case 1:
-        	std::cout << "x = " << x1;
-        break;
-    	case 2:
-        	cout << "x1 = " << x1 << endl;
-        	cout << "x2 = " << x2 << endl;
-        break;
-    }
+    std::cout << "Введите a: ";
+    std::cin >> a;
+    std::cout << "Введите b: ";
+    std::cin >> b;
+    std::cout << "Введите c: ";
+    std::cin >> c;
+    D = discriminant(a, b, c, &D);
+    answer(a, b, c, D, x1, x2);
     return 0;
 }
